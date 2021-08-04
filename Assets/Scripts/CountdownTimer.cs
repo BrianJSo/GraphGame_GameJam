5cb8143fe8fb2e55cca274de90bbf5a3e83ece97
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-08-04 17:12:22 +0800

    added pachinkoTime panel

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -7,6 +7,7 @@ public class CountdownTimer : MonoBehaviour
 {
     [SerializeField] private float startingTime = 10f;
     [SerializeField] private TextMeshProUGUI countDownText;
+    [SerializeField] private GameObject pachinkoTimePanel;
     private float currentTime = 0f;
 
     // Start is called before the first frame update
@@ -25,7 +26,12 @@ public class CountdownTimer : MonoBehaviour
 
         if(currentTime <= 0)
         {
-            currentTime = 0;
+            pachinkoTimePanel.SetActive(true);
+        }
+
+        if (currentTime <= -2)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Pachinko");
         }
     }
 }
